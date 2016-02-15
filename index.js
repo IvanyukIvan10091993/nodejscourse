a44036792d9a7959c5ec686a11c2ad53932e3a0f
IvanyukIvan10091993 <von.asar@gmail.com>
2016-02-15 11:03:35 +0300

    Added patriotism and function to clear log

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // Libraries
 var ansi = require('ansi');
 var colors = require('colors/safe');
-
 var cursor = ansi(process.stdout);
-
 // Variables
 var date,
     hour,
@@ -11,13 +9,17 @@ var date,
     second,
     temp;
 
+// A function to clear log
+function clearLog() {
+  return process.stdout.write('\033c');
+}
 // A function to convert a number to two-digit format
 function doubleNumber(number) {
     return (number > 9) ? number : '0' + number;
 }
-
 // A function to display time and beep
 function displayTime() {
+    clearLog();
     date = new Date();
     temp = date.getHours();
     hour = doubleNumber(temp);
@@ -26,9 +28,18 @@ function displayTime() {
     temp = date.getSeconds();
     second = doubleNumber(temp);
     console.log(colors.red(hour) + colors.grey(':') + colors.green(minute) + colors.grey(':') + colors.blue(second));
+    logPatriotism();
     cursor.beep();
 }
-
+// A function to log patriotism
+function logPatriotism() {
+    console.log(colors.white('Русские') + colors.grey(' и ') + colors.yellow('украинцы') + colors.grey(' - братья навек!'));
+    console.log(colors.white('Русские') + colors.grey(' и ') + colors.yellow('украинцы') + colors.grey(' - братья навек!'));
+    console.log(colors.blue('Русские') + colors.grey(' и ') + colors.yellow('украинцы') + colors.grey(' - братья навек!'));
+    console.log(colors.blue('Русские') + colors.grey(' и ') + colors.blue('украинцы') + colors.grey(' - братья навек!'));
+    console.log(colors.red('Русские') + colors.grey(' и ') + colors.blue('украинцы') + colors.grey(' - братья навек!'));
+    console.log(colors.red('Русские') + colors.grey(' и ') + colors.blue('украинцы') + colors.grey(' - братья навек!'));
+}
 // A function to repeat a function at a given interval
 function repeater (functionToRepeat, interval) {
   functionToRepeat();
