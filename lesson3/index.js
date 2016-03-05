da1dad0335b9401902b1d557eaa153d042d5eafd
IvanyukIvan10091993 <von.asar@gmail.com>
2016-03-06 00:15:56 +0300

    Removed test logs

--- lesson3/index.js
+++ lesson3/index.js
@@ -21,23 +21,9 @@ var a1_2 = {
   2: new Menu(false, 'Menu1.2.2', 'Menu1.2.2', a1[2])
 }
 
-/*
-console.log('\nOut of constructor calls: ');
-
-console.log('previousMenuObj of ' + a0.headerStr + ': ' + a0.previousMenuObj.headerStr);
-console.log('previousMenuObj of ' + a1[1].headerStr + ': ' + a1[1].previousMenuObj.headerStr);
-console.log('previousMenuObj of ' + a1[2].headerStr + ': ' + a1[2].previousMenuObj.headerStr);
-console.log('previousMenuObj of ' + a1_1[1].headerStr + ': ' + a1_1[1].previousMenuObj.headerStr);
-console.log('previousMenuObj of ' + a1_1[2].headerStr + ': ' + a1_1[2].previousMenuObj.headerStr);
-console.log('previousMenuObj of ' + a1_2[1].headerStr + ': ' + a1_2[1].previousMenuObj.headerStr);
-console.log('previousMenuObj of ' + a1_2[2].headerStr + ': ' + a1_2[2].previousMenuObj.headerStr);
-//*/
-
-//*
 a0.fillMenus(a1);
 a1[1].fillMenus(a1_1);
 a1[2].fillMenus(a1_2);
-//*/
 
 a0.actionFunc();
 
